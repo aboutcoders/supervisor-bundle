e030c253445d24da4e1b2ffc9fb453e24d5abec7
Wojciech Ciołko <w.ciolko@gmail.com>
2019-06-30 13:53:33 +0200

    Removed deprecation for symfony 3.4

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -19,20 +19,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 abstract class BaseController extends Controller
 {
-    /**
-     * Returns a JsonResponse that uses the serializer component if enabled, or json_encode.
-     *
-     * @ToDo To be removed when dependency on Symfony 2.7 is bumped
-     * @param mixed $data    The response data
-     * @param int   $status  The status code to use for the Response
-     * @param array $headers Array of extra headers to add
-     * @param array $context
-     * @return JsonResponse
-     */
-    protected function json($data, $status = 200, $headers = array(), $context = array())
-    {
-        return new JsonResponse($data, $status, $headers);
-    }
 
     /**
      * @return SupervisorManager
